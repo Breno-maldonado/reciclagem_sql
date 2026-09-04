54b69fad5814e0793c829371e6c6ec82b11d8f2d
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-09-04 12:00:12 -0300

    aula 01

---
+++ consultas/aula_01.sql
@@ -0,0 +1,3 @@
+SELECT * FROM HistoricoEmprego -- selecione tudo da tabela HistoricoEmprego
+ORDER BY salario DESC -- ordene a coluna salario de forma decrescente
+LIMIT 5 -- limite em apenas 5 resultados
