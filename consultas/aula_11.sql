0b498ba2f8754173c75a6d821d5e27745344615a
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-09-15 23:15:10 -0300

    aula 11

---
+++ consultas/aula_11.sql
@@ -0,0 +1,2 @@
+SELECT ('A pessoa colaboradora ' || nome || ' de CPF ' || cpf || ' possui o seguinte endereço ' || endereco) AS texto -- concatena as strings com as colunas, onde () armazena os valores, '' as strings e || concatena a string com as colunas
+FROM colaboradores -- da tabela colaboradores
