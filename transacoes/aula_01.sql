ff7ca731e13cfeff21642a1d75756e1ab4b6d9fd
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-09-20 23:20:23 -0300

    aula 01

---
+++ transacoes/aula_01.sql
@@ -0,0 +1,5 @@
+CREATE TABLE colaboradores -- cria uma tabela chamada colaboradores
+    id TEXT PRIMARY KEY,
+    nome VARCHAR 255,
+    cargo VARCHAR 255
+    datacontrato DATE -- passa as colunas que vão ser vinculadas a essa tabela 
