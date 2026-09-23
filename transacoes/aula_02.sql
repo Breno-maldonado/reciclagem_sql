48da4ae125fb884b2244b98c61b6bf7ec57dfff6
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-09-22 21:28:49 -0300

    aula 02

---
+++ transacoes/aula_02.sql
@@ -0,0 +1,13 @@
+SELECT rua,
+    bairro,
+    cidade,
+    estado,
+    cep -- seleciona essas 5 colunas
+FROM colaboradores -- da tabela colaboradores
+UNION -- e uni ela com a tabela fornecedores atraves das mesmas colunas
+SELECT rua,
+    bairro,
+    cidade,
+    estado,
+    cep -- seleciona essas 5 colunas
+FROM fornecedores -- da tabela fornecedores
