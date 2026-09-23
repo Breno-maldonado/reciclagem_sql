e26cf0e5ed54904f37f9623fd7573850f01d23e4
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-09-22 21:44:10 -0300

    aula 05

--- transacoes/aula_05.sql
+++ transacoes/aula_05.sql
@@ -3,4 +3,11 @@ FROM clientes -- da tabela clientes
 WHERE id = -- onde o id vai servir como subconsulta
     SELECT idclientes -- seleciona a coluna idclientes
     FROM pedidos -- da tabela pedidos
-    WHERE strftime '%m', datapedido = '01' -- onde na coluna datapedidos tenha no mes o valor '01', resultando no nome dos cliente referente ao mes 01
+    WHERE strftime '%m', datapedido = '01' -- onde na coluna datapedidos tenha no mes o valor '01', resultando no idcliente referente ao mes 01
+
+SELECT nome -- seleciona a coluna nome
+FROM clientes -- da tabela clientes
+WHERE id IN -- onde o id vai servir como subconsulta entre
+    SELECT idclientes -- seleciona a coluna idclientes
+    FROM pedidos -- da tabela pedidos
+    WHERE strftime '%m', datapedido = '01' -- onde na coluna datapedidos tenha no mes o valor '01', resultando no nome dos clientes referente ao mes 01
